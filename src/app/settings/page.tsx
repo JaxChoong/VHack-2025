0a51eb6d94bfe10cfdf3e6226f6b11ec87cc4e7e
k3mystra <programmer.coleslaw981@slmail.me>
2025-03-18 10:55:33 +0800

    fix: Change unchecked slider color to darker gray for better contrast

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -43,7 +43,7 @@ const settingsList = [
     children: <div>
       <label className="relative inline-flex items-center cursor-pointer">
         <input type="checkbox" className="sr-only peer" />
-        <div className={`w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full
+        <div className={`w-11 h-6 bg-gray-500 rounded-full peer peer-checked:after:translate-x-full
                   peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5
                   after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all
                   peer-checked:bg-blue-500`} />
@@ -82,7 +82,7 @@ const settingsList = [
       <div>
         <label className="relative inline-flex items-center cursor-pointer">
           <input type="checkbox" className="sr-only peer" />
-          <div className={`w-11 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full
+          <div className={`w-11 h-6 bg-gray-500 rounded-full peer peer-checked:after:translate-x-full
             peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5
             after:bg-white after:border after:rounded-full after:h-5 after:w-5 after:transition-all
             peer-checked:bg-purple-500`}></div>
